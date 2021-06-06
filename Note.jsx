27c170a6e4e5f0266f78c4c9f987bc232249ed13
Boya Pushpalatha <60311622+Pushpalatha76@users.noreply.github.com>
2021-06-06 14:20:58 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>This was a simple project using React.js and Javascript...By Boya Pushpalatha.... @ShapeAI@Shaurya Sinha</p>
+    </div>
+  );
+}
+
+export default Note;
